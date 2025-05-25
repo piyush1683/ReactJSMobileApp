13a883e8f2f85911681b3e2d6b200fb021a617a9
piyush1683 <piyushkumar1983@gmail.com>
2025-05-25 11:17:58 +0530

    Integrated with local trade API

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,11 +1,21 @@
+import React, { useState, useEffect } from 'react';
+
 export default function Button() {
   return (
-    <button onClick={handleClick}>
-      This is a button and it is clickable!
+    <button onClick={() => handleClick()}>
+        Get All trade information
     </button>
   );
 }
 
 function handleClick() {
-  alert("Button clicked!");
+    alert('Fetching trade information...');
+    console.log('Fetching trade information...');
+      try{
+        fetch('http://localhost:8080/trade').then(resp => resp.json())
+        .then(data => {alert(JSON.stringify(data))});
+        }catch (error) {
+            console.error('Error fetching trade information:', error);
+            alert('Error fetching trade information');
+        }; 
 }
